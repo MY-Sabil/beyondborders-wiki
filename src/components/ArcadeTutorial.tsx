04aea6743a0a676d0020c75722e5d2a1e387a0e8
Muhaiminul Yeamin <sabilsabil121@gmail.com>
2024-01-24 19:21:51 +0600

    added interactive tutorial for contributing

---
+++ src/components/ArcadeTutorial.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ArcadeTutorial = () => (
+  <div style={{ position: 'relative', paddingBottom: 'calc(70.0168918918919% + 41px)', height: 0 }}>
+    <iframe
+      src="https://demo.arcade.software/xZmLF84MOsTwQQ0PygwF?embed"
+      title="Beyond Borders Wiki"
+      loading="lazy"
+      allowFullScreen
+      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', colorScheme: 'light', border: '0' }}
+    ></iframe>
+  </div>
+);
+
+export default ArcadeTutorial;
--- src/pages/contribute.md
+++
@@ -1,5 +0,0 @@
-# Contribution Guide
-
-Follow this guideline for now, [Contributing to Beyond Borders Wiki 🌐](https://github.com/MY-Sabil/beyondborders-wiki/blob/main/CONTRIBUTING.md).
-
-*(Will be updated soon :D)*
